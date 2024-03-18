aa68f22babbaf5c76e247bee091dabcb40770c7c
Pedro Oliveira <pedrophmo1205@gmail.com>
2024-03-18 14:31:15 -0300

    minor changes

--- Codigo/TOQUE.DE.CHEF/TOQUE.DE.CHEF/obj/Debug/net8.0/TOQUE.DE.CHEF.AssemblyInfo.cs
+++ Codigo/TOQUE.DE.CHEF/TOQUE.DE.CHEF/obj/Debug/net8.0/TOQUE.DE.CHEF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TOQUE.DE.CHEF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae6b4fe0083d74708cee7cf56a11d7ac94cbcf73")]
 [assembly: System.Reflection.AssemblyProductAttribute("TOQUE.DE.CHEF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TOQUE.DE.CHEF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
