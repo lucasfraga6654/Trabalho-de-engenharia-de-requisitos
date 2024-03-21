63a5aaf71688760d53c14bdd595f9720b81e1337
rodrigoribas1 <rodrigoribas@outlook.com>
2024-03-21 00:07:46 -0300

    Delete Codigo/TOQUE.DE.CHEF/TOQUE.DE.CHEF/Models directory

--- Codigo/TOQUE.DE.CHEF/TOQUE.DE.CHEF/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TOQUE.DE.CHEF.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
